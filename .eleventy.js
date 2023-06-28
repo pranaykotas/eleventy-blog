9437233bdc38a96babaf6ff15314087b1948e6bb
Pranay Kotasthane <pranay-karma@MacBook-Air-2.local>
2023-06-28 20:28:47 +0530

    Initial version of the site

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const { DateTime } = require('luxon');
+
+
+module.exports = function (eleventyConfig) {
+  // Copy the `css` directory to the output
+  eleventyConfig.addPassthroughCopy('css');
+
+  // Watch the `css` directory for changes
+  eleventyConfig.addWatchTarget('css');
+
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+      'dd LLL yyyy'
+    );
+  });
+
+};
